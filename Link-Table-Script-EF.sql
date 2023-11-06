06a455c808641e41b7e82af0bfe856db54767141
DiegoHZ5 <49615431+Diego5HZ@users.noreply.github.com>
2023-11-05 23:19:17 -0500

    se agrga device token

--- Link-Table-Script-EF.sql
+++ Link-Table-Script-EF.sql
@@ -27,7 +27,7 @@ CREATE TABLE users (
 	code varchar(40) NOT NULL,
 	username nvarchar(40),
 	token nvarchar(max),
-	notification_token nvarchar(40) NULL,
+	device_token nvarchar(40) NULL,
 	attempt int,
 	creation_date datetime,
 	last_update_date datetime NULL,
